ce37ae23d3f77be3f351fe704b5d551a8a422061
gdan <1173882990@qq.com>
2026-01-06 10:10:25 +0700

    修改

--- client/vite.config.js
+++ client/vite.config.js
@@ -18,6 +18,8 @@ export default defineConfig({
     server: {
         port: 8080,
         open: true,
+        host: '0.0.0.0',
+        allowedHosts: 'all',
     },
     build: {
         outDir: 'dist',
